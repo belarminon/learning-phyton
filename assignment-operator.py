fd51b9cc650bcb3d08391c28944f8a8b57e03a17
belarminon <35656300+belarminon@users.noreply.github.com>
2024-05-29 16:30:51 +0000

    add assignment operator file

---
+++ assignment-operator.py
@@ -0,0 +1,17 @@
+balance = 1000 #assign value 1000 into balance variable
+print(balance)
+
+balance = 500 #change value 1000 of balance variable into 500
+print(balance)
+
+# Assagn with arithmetic operators
+balance += 100
+print(balance)
+balance -= 200
+print(balance)
+balance /= 50
+print(balance)
+balance //= 2
+print(balance)
+balance *= 5
+print(balance)
