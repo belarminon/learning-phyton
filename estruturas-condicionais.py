4af4d2b722fc1512ad812324c5d237b23ab7e1ee
belarminon <35656300+belarminon@users.noreply.github.com>
2024-05-30 00:01:17 +0000

    condicional if ilif e else

--- estruturas-condicionais.py
+++ estruturas-condicionais.py
@@ -1,8 +1,11 @@
 MAIOR_IDADE = 18
+IDADE_ESPECIAL = 17
 
 idade = int(input("Informe a tua idade: "))
 
 if idade >= MAIOR_IDADE:
     print("Maior de idade, pode consumir bebida alcoolica.")
+elif idade == IDADE_ESPECIAL:
+    print("Pode segurar a latinha de cerveja rsrsr.")
 else:
     print("Ixi, é menor de idade.")
