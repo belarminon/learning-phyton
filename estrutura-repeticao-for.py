719d298a850f0493b63aab1ecec0855bca895045
belarminon <35656300+belarminon@users.noreply.github.com>
2024-05-30 13:34:08 +0000

    for with built-in function range

--- estrutura-repeticao-for.py
+++ estrutura-repeticao-for.py
@@ -2,9 +2,20 @@
 texto = input("Informe um texto: ")
 VOGAIS = "AEIOU"
 
+# Exemple using an interable
+
 for letra in texto:
     if letra.upper() in VOGAIS:
         print(letra, end="")
 else:
     print()
     print("Executa no final do laço")
+
+# Exemple using a buildt-in function RANGE
+
+for number in range(0,11):
+    print(number, end=" ")
+    print()
+
+for number in range(0, 51, 5):
+    print(number, end=" ")
