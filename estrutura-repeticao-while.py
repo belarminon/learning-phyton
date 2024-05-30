5bba2d22545998ff504330f73fea915eca4e2804
belarminon <35656300+belarminon@users.noreply.github.com>
2024-05-30 13:40:23 +0000

    add estrutura-repeticao-while and contexts

---
+++ estrutura-repeticao-while.py
@@ -0,0 +1,14 @@
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] Sacar \n[2] Extrato \n[0] Sair \n: "))
+
+    if opcao == 1:
+        print("sacado...")
+    elif opcao == 2:
+        print("Exibindo o extrato...")
+    elif opcao == 0:
+        # sys.exit
+        print("implementar a function para sair")
+    else:
+        print("Opção inválida")
