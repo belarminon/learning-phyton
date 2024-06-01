9c8be3841931b5d3237adc268ee086557e60aa9b
belarminon <35656300+belarminon@users.noreply.github.com>
2024-06-01 13:58:56 +0000

    add string fatiament and its contexts

---
+++ string_fatiamento.py
@@ -0,0 +1,13 @@
+# Fatiamento de strings é uma tecnica utulizada para retornar sbstrings (partes da string original), informando inicio 
+# (start), fim (stop) e passo (step): [start: stop[,step]].
+
+name = "Belarmino Nicolau Monteiro Simao"
+
+print(name[0])
+print(name[:9])
+print(name[10:])
+print(name[10:20])
+print(name[10:20:2])
+print(name[:])
+print(name[::-1])
+print(name[-1])
