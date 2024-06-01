300155a26d086d82a366365d769d75c6f971779d
belarminon <35656300+belarminon@users.noreply.github.com>
2024-06-01 12:42:04 +0000

    interpolation with old style %

---
+++ string_interpolation.py
@@ -0,0 +1,8 @@
+name = "Belarmino"
+age = 43
+profission = "Developer"
+language = "Phyton"
+
+# Old style %
+
+print("Hello, I'm %s, I'm %d years old, I work as %s and my hard skill is %s." %(name, age, profission, language))
