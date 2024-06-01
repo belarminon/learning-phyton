a78ca16fd26f6fc80638770616ddab1d40c76102
belarminon <35656300+belarminon@users.noreply.github.com>
2024-06-01 12:34:55 +0000

    add string_first file and context

---
+++ string_first.py
@@ -0,0 +1,20 @@
+name = "BELArminon"
+
+print(name.upper())
+print(name.lower())
+print(name.title())
+print()
+
+texto = "    Hello World!     "
+print(texto + ".")
+print(texto.strip() + ".")
+print(texto.rstrip() + ".")
+print(texto.lstrip() + ".")
+
+menu = "Phyton"
+
+print("##########" + menu + "##########")
+print(menu.center(26))
+print(menu.center(26, "#"))
+
+print("-".join(menu))
