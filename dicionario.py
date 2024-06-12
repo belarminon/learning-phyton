a792da583fc2da4f9c4daf0587becf51dca88518
belarminon <35656300+belarminon@users.noreply.github.com>
2024-06-12 15:39:03 +0000

    define values and add new key

--- dicionario.py
+++ dicionario.py
@@ -1,1 +1,11 @@
 # Umio dicionario é um conjunto não-ordernado de pares chave:valor, onde as chaves são unicas em uma dada instancia do dicionário. Dicionário são limitados por chaves: {}, e contém uma lista de pares chave:valor separada por vírgulas.
+
+#define a values
+person = {"name":"Belarmino", "age":43}
+print(person)
+person = dict(name="Belarmino", age=43)
+print(person)
+
+#add ney key "phone"
+person["phone"] = "(12) 98845-0080"
+print(person)
